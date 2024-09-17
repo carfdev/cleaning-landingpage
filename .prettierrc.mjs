f0a85c2e17672c9bff7ef6091254f8dee935d8cb
carfdev <jose.carlos@fdez.se>
2024-09-17 13:29:16 +0200

    Add: eslint

---
+++ .prettierrc.mjs
@@ -0,0 +1,19 @@
+/** @type {import("prettier").Config} */
+export default {
+  plugins: ["prettier-plugin-astro"],
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+        singleQuote: true,
+        trailingComma: "none",
+        bracketSpacing: true,
+        printWidth: 120,
+        tabWidth: 2,
+        useTabs: false,
+        semi: false,
+      },
+    },
+  ],
+};
